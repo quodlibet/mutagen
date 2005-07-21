104387c8304ee450a3384a9175e924b716dfbdb4
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-07-21 05:50:09 +0000

    Installer.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+setup(name="mutagen", version="0",
+      url="http://www.sacredchao.net/quodlibet",
+      description="ID3 reading/writing support",
+      author="Michael Urman",
+      license = "GNU GPL v2",
+      packages = ["mutagen"],
+    )
