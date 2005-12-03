e2bd63aae343d77a3f8f6a16e31e36e737bf104d
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-03 03:28:22 +0000

    Doc updates. id3inspect -> taginspect, print apev2 information.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup, Extension
 setup(name="mutagen", version="0.0",
-      url="http://www.sacredchao.net/quodlibet/wiki/Mutagen",
-      description="ID3v 1.1, 2.2, 2.3, 2.4 reader / 1.1, 2.4 writer",
+      url="http://www.sacredchao.net/quodlibet/wiki/Development/Mutagen",
+      description="ID3v 1.1, 2.2, 2.3, 2.4, APEv2 reader / 1.1, 2.4, APEv2 writer",
       author="Michael Urman",
       license = "GNU GPL v2",
       packages = ["mutagen"],
