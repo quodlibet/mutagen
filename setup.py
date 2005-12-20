7a88bc07560ddab491329aad2b2a5532450efa8f
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-20 05:47:11 +0000

    Docs: Mention FLAC support.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 setup(name="mutagen", version="0.0",
       url="http://www.sacredchao.net/quodlibet/wiki/Development/Mutagen",
-      description="ID3v 1.1, 2.2, 2.3, 2.4, APEv2 reader / 1.1, 2.4, APEv2 writer",
+      description="ID3v1.1, 2.2, 2.3, 2.4, APEv2, FLAC reader / 1.1, 2.4, APEv2, FLAC writer",
       author="Michael Urman",
       license = "GNU GPL v2",
       packages = ["mutagen"],
