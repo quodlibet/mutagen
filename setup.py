c0bca27b276e8e183cb3983d1eae4627383d2754
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-02-21 06:05:42 +0000

    Version 0.9.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-setup(name="mutagen", version="0.0",
+setup(name="mutagen", version="0.9",
       url="http://www.sacredchao.net/quodlibet/wiki/Development/Mutagen",
       description="ID3v1.1, 2.2, 2.3, 2.4, APEv2, FLAC reader / 1.1, 2.4, APEv2, FLAC writer",
       author="Michael Urman",
