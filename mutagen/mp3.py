a54dc2bef7a49b3045ddd1ee7a92ae6070fd57e1
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-03-12 23:25:51 +0000

    MPEGInfo: Minor optimizations.

--- mutagen/mp3.py
+++ mutagen/mp3.py
@@ -92,7 +92,7 @@ class MPEGInfo(object):
         while 0 <= frame_1 <= len(data) - 4:
             frame_data = struct.unpack(">I", data[frame_1:frame_1 + 4])[0]
             if (frame_data >> 16) & 0xE0 != 0xE0:
-                frame_1 = data.find("\xff", frame_1 + 1)
+                frame_1 = data.find("\xff", frame_1 + 2)
             else:
                 version = (frame_data >> 19) & 0x3
                 layer = (frame_data >> 17) & 0x3
@@ -108,7 +108,7 @@ class MPEGInfo(object):
                 emphasis = (frame_data >> 0) & 0x3
                 if (version == 1 or layer == 0 or sample_rate == 0x3 or
                     bitrate == 0 or bitrate == 0xF):
-                    frame_1 = data.find("\xff", frame_1 + 1)
+                    frame_1 = data.find("\xff", frame_1 + 2)
                 else: break
         else:
             raise HeaderNotFoundError("can't sync to an MPEG frame")
@@ -158,7 +158,7 @@ class MPEGInfo(object):
                 self.length = samples / self.sample_rate
             if flags & 0x2:
                 bytes = struct.unpack('>I', data[xing + 12:xing + 16])[0]
-                self.bitrate = int((bytes * 8.0 / self.length))
+                self.bitrate = (bytes * 8) // self.length
 
 class MP3(FileType):
     """An MPEG audio (usually MPEG-1 Layer 3) object, optionally
