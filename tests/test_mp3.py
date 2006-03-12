6af364cf52c24f4e401c7509e08e356e270c4933
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-03-12 20:07:43 +0000

    Test mutagen.mp3.MP3 on a file without a v2 tag as well.

--- tests/test_mp3.py
+++ tests/test_mp3.py
@@ -6,18 +6,26 @@ from mutagen.id3 import ID3
 
 class TMP3(TestCase):
     silence = os.path.join('tests', 'data', 'silence-44-s.mp3')
+    silence_nov2 = os.path.join('tests', 'data', 'silence-44-s-v1.mp3')
+
     def setUp(self):
         self.mp3 = MP3(self.silence)
+        self.mp3_2 = MP3(self.silence_nov2)
 
     def test_id3(self):
         self.failUnlessEqual(self.mp3.tags, ID3(self.silence))
+        self.failUnlessEqual(self.mp3_2.tags, ID3(self.silence_nov2))
 
     def test_length(self):
         self.failUnlessEqual(int(round(self.mp3.info.length)), 4)
+        self.failUnlessEqual(int(round(self.mp3_2.info.length)), 4)
     def test_version(self):
         self.failUnlessEqual(self.mp3.info.version, 1)
+        self.failUnlessEqual(self.mp3_2.info.version, 1)
     def test_layer(self):
         self.failUnlessEqual(self.mp3.info.layer, 3)
+        self.failUnlessEqual(self.mp3_2.info.layer, 3)
     def test_bitrate(self):
         self.failUnlessEqual(self.mp3.info.bitrate, 32000)
+        self.failUnlessEqual(self.mp3_2.info.bitrate, 32000)
 registerCase(TMP3)
