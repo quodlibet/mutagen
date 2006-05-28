0252eef4cfec6e9cf7d50d247b082602bb83479a
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-05-28 01:26:30 +0000

    Documentation updates. Install scripts and man pages using distutils.

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import os
+import glob
+
 from distutils.core import setup, Extension
+
+if os.name == "posix":
+    data_files = [('share/man/man1', glob.glob("man/*.1"))]
+else:
+    data_files = []
+
 setup(name="mutagen", version="1.3",
       url="http://www.sacredchao.net/quodlibet/wiki/Development/Mutagen",
       description="read and write ID3v1/ID3v2/APEv2/FLAC/Ogg audio tags",
@@ -8,6 +17,8 @@ setup(name="mutagen", version="1.3",
       author_email="quodlibet@lists.sacredchao.net",
       license="GNU GPL v2",
       packages=["mutagen"],
+      data_files=data_files,
+      scripts=glob.glob("tools/m*[!~]"),
       long_description="""\
 Mutagen is a Python module to handle audio metadata. It supports
 reading ID3 (all versions), APEv2, Ogg Vorbis, FLAC, and Ogg FLAC, and
