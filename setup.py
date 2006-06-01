2d65fddf1458b6a864264478ac60f7e2bd84fc2a
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-06-01 03:55:05 +0000

    setup.py:  * clean: Remove pyc and pyo files.  * test: Run test suite, remove _sanity.sh.

--- setup.py
+++ setup.py
@@ -1,16 +1,54 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import glob
 
-from distutils.core import setup, Extension
+from distutils.core import setup, Extension, Command
+
+from distutils.command.clean import clean as distutils_clean
+
+class clean(distutils_clean):
+    def run(self):
+        # In addition to what the normal clean run does, remove pyc
+        # and pyo files from the source tree.
+        distutils_clean.run(self)
+        def should_remove(filename):
+            if filename.lower()[-4:] in [".pyc", ".pyo"]:
+                return True
+            else:
+                return False
+        for pathname, dirs, files in os.walk(os.path.dirname(__file__)):
+            for filename in filter(should_remove, files):
+                try: os.unlink(os.path.join(pathname, filename))
+                except EnvironmentError, err:
+                    print str(err)
+
+class test_cmd(Command):
+    description = "run automated tests"
+    user_options = [
+        ("to-run=", None, "list of tests to run (default all)")
+        ]
+
+    def initialize_options(self):
+        self.to_run = []
+
+    def finalize_options(self):
+        if self.to_run:
+            self.to_run = self.to_run.split(",")
+
+    def run(self):
+        import tests
+        if tests.unit(self.to_run):
+            raise SystemExit("Test failures are listed above.")
 
 if os.name == "posix":
     data_files = [('share/man/man1', glob.glob("man/*.1"))]
 else:
     data_files = []
 
-setup(name="mutagen", version="1.3",
+setup(cmdclass={'clean': clean, 'test': test_cmd},
+      name="mutagen", version="1.3",
       url="http://www.sacredchao.net/quodlibet/wiki/Development/Mutagen",
       description="read and write ID3v1/ID3v2/APEv2/FLAC/Ogg audio tags",
       author="Michael Urman",
