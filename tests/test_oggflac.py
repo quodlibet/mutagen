fc01a98627fc0872831829ed18a2ebfec9994b0d
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-06-03 00:51:27 +0000

    TOggFLAC: Split up and rename test_vorbiscomment.

--- tests/test_oggflac.py
+++ tests/test_oggflac.py
@@ -23,32 +23,41 @@ class TOggFLAC(TOggFileType):
             self.audio.tags.vendor.startswith("reference libFLAC"))
         self.failUnlessRaises(KeyError, self.audio.tags.__getitem__, "vendor")
 
-    def test_vorbiscomment(self):
+    def test_flac_reference_simple_save(self):
         self.audio.save()
-        badval = os.system("tools/notarealprogram 2> /dev/null")
+        self.scan_file()
         value = os.system("flac --ogg -t %s 2> /dev/null" % self.filename)
-        self.failIf(value and value != badval)
-        if value == badval:
-            sys.stdout.write("\bS")
-            return
+        self.failIf(value and value != NOTFOUND)
 
+    def test_flac_reference_really_big(self):
         self.test_really_big()
         self.audio.save()
         self.scan_file()
         value = os.system("flac --ogg -t %s 2> /dev/null" % self.filename)
-        self.failIf(value and value != badval)
+        self.failIf(value and value != NOTFOUND)
 
+    def test_flac_reference_delete(self):
         self.audio.delete()
         self.scan_file()
         value = os.system("flac --ogg -t %s 2> /dev/null" % self.filename)
-        self.failIf(value and value != badval)
-
+        self.failIf(value and value != NOTFOUND)
+ 
+    def test_flac_reference_medium_sized(self):
         self.audio["foobar"] = "foobar" * 1000
         self.audio.save()
         self.scan_file()
         value = os.system("flac --ogg -t %s 2> /dev/null" % self.filename)
-        self.failIf(value and value != badval)
+        self.failIf(value and value != NOTFOUND)
 
+    def test_flac_reference_delete_readd(self):
+        self.audio.delete()
+        self.audio.tags.clear()
+        self.audio["foobar"] = "foobar" * 1000
+        self.audio.save()
+        self.scan_file()
+        value = os.system("flac --ogg -t %s 2> /dev/null" % self.filename)
+        self.failIf(value and value != NOTFOUND)
+ 
     def test_not_my_ogg(self):
         fn = os.path.join('tests', 'data', 'empty.ogg')
         self.failUnlessRaises(IOError, type(self.audio), fn)
@@ -56,3 +65,8 @@ class TOggFLAC(TOggFileType):
         self.failUnlessRaises(IOError, self.audio.delete, fn)
 
 add(TOggFLAC)
+
+NOTFOUND = os.system("tools/notarealprogram 2> /dev/null")
+
+if os.system("flac 2> /dev/null > /dev/null") == NOTFOUND:
+    print "WARNING: Skipping FLAC reference tests."
