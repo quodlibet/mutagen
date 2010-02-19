f74555ae56bb54a63e23e1c625e2d4a2667501eb
Joe Wreschnig <joe.wreschnig@gmail.com>
2010-02-19 06:24:32 +0000

    1.19 release.

--- mutagen/__init__.py
+++ mutagen/__init__.py
@@ -21,7 +21,7 @@ depending on tag or format. They may also be entirely different objects
 for certain keys, again depending on format.
 """
 
-version = (1, 18, -1)
+version = (1, 19, -1)
 version_string = ".".join(map(str, version))
 
 import warnings
