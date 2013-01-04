72bde253f2bcb8847c9c61a47d6e99b0a940c0c3
Christoph Reiter <reiter.christoph@gmail.com>
2013-01-04 12:05:54 +0100

    Mutagen 1.16

--- mutagen/__init__.py
+++ mutagen/__init__.py
@@ -21,7 +21,7 @@ depending on tag or format. They may also be entirely different objects
 for certain keys, again depending on format.
 """
 
-version = (1, 16, -1)
+version = (1, 16)
 version_string = ".".join(map(str, version))
 
 import warnings
