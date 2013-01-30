fd468f94c633a58f8769ea9c4d4f6bc410064c7f
Christoph Reiter <reiter.christoph@gmail.com>
2013-01-30 18:40:55 +0100

    Remove svn version check

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -218,23 +218,3 @@ class TFileUpperExt(TestCase):
             os.unlink(path)
 
 add(TFileUpperExt)
-
-class TMutagen(TestCase):
-    uses_mmap = False
-
-    def test_not_prerelease(self):
-        from mutagen import version
-        from os.path import dirname, basename, abspath
-        trunk = dirname(dirname(abspath(__file__)))
-        definitely_trunk = os.path.join(trunk, "..", "trunk")
-        if os.path.isdir(definitely_trunk):
-            if basename(trunk) != "trunk":
-                self.failIf(
-                    version[-1] < 0, "mutagen.version is a prerelease.")
-            else:
-                self.failIf(
-                    version[-1] >= 0, "mutagen.version is not a prerelease.")
-        else:
-            print "WARNING: Unable to check version correctness."
-            print "WARNING: %r does not exist." % definitely_trunk
-add(TMutagen)
