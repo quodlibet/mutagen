7420b5f891d801fcd5e04e3d3de16cf200ef16c6
Christoph Reiter <reiter.christoph@gmail.com>
2014-08-12 12:51:01 +0200

    wavpack: Fix length calculation if sample count is missing in the header (Fixes issue #180)

--- tests/test_wavpack.py
+++ tests/test_wavpack.py
@@ -8,6 +8,9 @@ class TWavPack(TestCase):
     def setUp(self):
         self.audio = WavPack(os.path.join("tests", "data", "silence-44-s.wv"))
 
+    def test_version(self):
+        self.failUnlessEqual(self.audio.info.version, 0x403)
+
     def test_channels(self):
         self.failUnlessEqual(self.audio.info.channels, 2)
 
@@ -28,3 +31,29 @@ class TWavPack(TestCase):
         self.failUnless("audio/x-wavpack" in self.audio.mime)
 
 add(TWavPack)
+
+
+class TWavPackNoLength(TestCase):
+
+    def setUp(self):
+        self.audio = WavPack(os.path.join("tests", "data", "no_length.wv"))
+
+    def test_version(self):
+        self.failUnlessEqual(self.audio.info.version, 0x407)
+
+    def test_channels(self):
+        self.failUnlessEqual(self.audio.info.channels, 2)
+
+    def test_sample_rate(self):
+        self.failUnlessEqual(self.audio.info.sample_rate, 44100)
+
+    def test_length(self):
+        self.failUnlessAlmostEqual(self.audio.info.length, 3.705, 3)
+
+    def test_pprint(self):
+        self.audio.pprint()
+
+    def test_mime(self):
+        self.failUnless("audio/x-wavpack" in self.audio.mime)
+
+add(TWavPackNoLength)
