2044b9a76aff9dc0fd1d555578f7924c363f1c7c
Christoph Reiter <reiter.christoph@gmail.com>
2014-08-18 09:24:21 +0200

    add tests

---
+++ tests/quality/test_pep8.py
@@ -0,0 +1,73 @@
+# Copyright 2013,2014 Christoph Reiter
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+
+import os
+import glob
+import subprocess
+import unittest
+
+from tests import TestCase, add
+
+PEP8_NAME = "pep8"
+
+has_pep8 = True
+try:
+    subprocess.check_output([PEP8_NAME, "--version"], stderr=subprocess.STDOUT)
+except OSError:
+    has_pep8 = False
+
+
+class TPEP8(TestCase):
+    IGNORE = ["E12"]
+
+    def _run(self, path, ignore=None):
+        if ignore is None:
+            ignore = []
+        ignore += self.IGNORE
+
+        p = subprocess.Popen(
+            [PEP8_NAME, "--ignore=" + ",".join(ignore), path],
+            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        class Future(object):
+
+            def __init__(self, p):
+                self.p = p
+
+            def result(self):
+                if self.p.wait() != 0:
+                    return self.p.communicate()
+
+        return Future(p)
+
+    def _run_package(self, mod, ignore=None):
+        path = mod.__path__[0]
+        files = glob.glob(os.path.join(path, "*.py"))
+        assert files
+        futures = []
+        for file_ in files:
+            futures.append(self._run(file_, ignore))
+
+        errors = []
+        for future in futures:
+            status = future.result()
+            if status is not None:
+                errors.append(status[0].decode("utf-8"))
+
+        if errors:
+            raise Exception("\n".join(errors))
+
+    def test_main_package(self):
+        import mutagen
+        self._run_package(mutagen)
+
+    def test_tests(self):
+        import tests
+        self._run_package(tests)
+
+
+if has_pep8:
+    add(TPEP8)
