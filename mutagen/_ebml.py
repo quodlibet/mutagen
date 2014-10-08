8acbb3ce55a9ade348147b39eb2729b63bde971f
Ben Ockmore <ben.sput@gmail.com>
2014-10-08 13:51:59 +0100

    _ebml.py: Significantly improved speed by reading all elements from the file. Also made it possible to create elements externally.

--- mutagen/_ebml.py
+++ mutagen/_ebml.py
@@ -112,7 +112,7 @@ class ElementSpec(object):
     the element is mandatory within the parent and whether multiple
     elements are allowed.
     """
-    
+
     def __init__(self, id, name, element_type, range=None,
                  default=None, mandatory=True, multiple=False):
         self.id = id
@@ -122,49 +122,70 @@ class ElementSpec(object):
         self.default = default
         self.mandatory = mandatory
         self.multiple = multiple
-        
-    def create_element(self, e_id, d_size, data):
+
+    def create_element(self, e_id, d_size, dataobj):
         """Creates a new element using this specification."""
-        
-        return self.type(e_id, d_size, data)
-        
+
+        return self.type.from_buffer(e_id, d_size, dataobj)
+
 
 indentation = 0
 
 class Element(object):
-    def __init__(self, element_id, data_size, data):
+    def __init__(self, element_id, value):
+        print("Creating element {} ({})".format(type(self), element_id))
         self.id = element_id
-        self.data_size = data_size
-        self.data = data
 
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        raise NotImplementedError
+
+    @classmethod
+    def from_value(cls, element_id, value):
+        raise NotImplementedError
 
 class VoidElement(Element):
     def read(self, dataobj):
         pass
 
 class UnsignedInteger(long_, Element):
-    def __new__(cls, element_id, data_size, data):
+    def __new__(cls, element_id, value):
         # Pad the raw data to 8 octets
-        value = (b'\x00'*8 + data)[-8:]
-        return long_.__new__(UnsignedInteger, cdata.ulonglong_be(value))
+        return long_.__new__(UnsignedInteger, value)
 
-    def write(self):
-        self.data = cdata.to_ulonglong_be(self.val).strip(b'\x00')
-        self.data_size = len(data)
-        return self.id.write() + self.data_size.write() + self.data
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        data = dataobj.read(data_size)
+        data = (b'\x00'*8 + data)[-8:]
+        return cls(element_id, cdata.ulonglong_be(data))
+
+#    def write(self):
+#        self.data = cdata.to_ulonglong_be(self.val).strip(b'\x00')
+#        self.data_size = len(data)
+#        return self.id.write() + self.data_size.write() + self.data
 
 class String(text_type, Element):
-    def __new__(cls, element_id, data_size, data):
-        return text_type.__new__(String, data.strip(b'\x00').decode('ascii'))
+    def __new__(cls, element_id, value):
+        return text_type.__new__(String, value)
 
-    def write(self):
-        self.data = self.val.encode('ascii')
-        self.data_size = len(data)
-        return self.id.write() + self.data_size.write() + self.data
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        data = dataobj.read(data_size)
+        return cls(element_id, data.strip(b'\x00').decode('ascii'))
+
+    #def write(self):
+    #    self.data = self.val.encode('ascii')
+    #    self.data_size = len(data)
+    #    return self.id.write() + self.data_size.write() + self.data
 
 class UTF8String(text_type, Element):
-    def __new__(cls, element_id, data_size, data):
-        return text_type.__new__(UTF8String, data.strip(b'\x00').decode('utf-8'))
+    def __new__(cls, element_id, value):
+        return text_type.__new__(UTF8String, value)
+
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        data = dataobj.read(data_size)
+        return cls(element_id, data.strip(b'\x00').decode('utf-8'))
 
     def write(self):
         self.data = self.val.encode('utf-8')
@@ -172,8 +193,12 @@ class UTF8String(text_type, Element):
         return self.id.write() + self.data_size.write() + self.data
 
 class Binary(bytes, Element):
-    def __new__(cls, element_id, data_size, data):
-        return bytes.__new__(Binary, data)
+    def __new__(cls, element_id, value):
+        return bytes.__new__(Binary, value)
+
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        return cls(element_id, dataobj.read(data_size))
 
     def write(self):
         self.data = self.val
@@ -183,18 +208,28 @@ class Binary(bytes, Element):
 class MasterElement(Element):
     # Child elements, in format {ID: ElementSpec(id, name, type)}
     _child_specs = {}
-    
-    def __init__(self, element_id, data_size, data):
-        super(MasterElement, self).__init__(element_id, data_size, data)
+
+    def __init__(self, element_id, value):
+        super(MasterElement, self).__init__(element_id, value)
 
         self.children = []
-        self.dataobj = io.BytesIO(data)
-        self.read()
 
-    def read(self):
-        while 1:
+    @classmethod
+    def from_buffer(cls, element_id, data_size, dataobj):
+        result = cls(element_id, None)
+        result.read(data_size, dataobj)
+        return result
+
+    def read(self, data_size, dataobj):
+        # Improve this later on...
+        if data_size is not None:
+            end_position = dataobj.tell() + data_size
+        else:
+            end_position = -1
+
+        while dataobj.tell() != end_position:
             try:
-                e_id, d_size, data = self.read_element()
+                e_id, d_size = self.read_element_info(dataobj)
             except EOFError:
                 break
 
@@ -202,60 +237,69 @@ class MasterElement(Element):
             if spec is None:
                 # Append tuple indicating element was unrecognised, then
                 # move on to the next element.
-                self.children.append((e_id, d_size, data))
+                self.children.append((e_id, d_size))
+                dataobj.seek(d_size, 1)
                 continue
-                
+
             if not spec.multiple:
                 # Check that there are no elements with the same ID
                 if any(child.id == spec.id for child in self.children):
                     raise Exception("Attempt to set multiple values for non multi element")
 
-            self.children.append(spec.create_element(e_id, d_size, data))
+            current_pos = dataobj.tell()
+            self.children.append(spec.create_element(e_id, d_size, dataobj))
+            # Seek to the correct location, to prevent any read issues
+            dataobj.seek(current_pos + d_size)
 
+        self._check_mandatory_children()
+        self._set_attributes()
+
+    def _check_mandatory_children(self):
         # Check that all mandatory elements are set, otherwise raise Exception
         mandatory_specs = [
             k for k,v in self._child_specs.items() if v.mandatory
         ]
-                
+
         for child in self.children:
             if isinstance(child, Element):
                 if child.id in mandatory_specs:
                     mandatory_specs.remove(child.id)
-        
+
         if mandatory_specs:
             missing = ", ".join("0x{:02x}".format(m) for m in mandatory_specs)
             raise Exception("The following mandatory elements are missing: {}".format(missing))
-            
+
+    def _set_attributes(self):
         # Set attributes
         for child in self.children:
-            if isinstance(child, Element):
-                spec = self._child_specs[child.id]
-                
-                try:
-                    existing_element = getattr(self, spec.name)
-                except AttributeError:
-                    if spec.multiple:
-                        child = [child]
-                        
-                    setattr(self, spec.name, child)
-                else:
-                    # Since we know the children are all valid already,
-                    # this must be a multi-element
-                    existing_element.append(child)
-                        
-            
+            if not isinstance(child, Element):
+                continue
 
-    def read_element(self):
-        if self.dataobj.read(1):
-            self.dataobj.seek(-1, 1)
+            spec = self._child_specs[child.id]
+
+            try:
+                existing_element = getattr(self, spec.name)
+            except AttributeError:
+                if spec.multiple:
+                    child = [child]
+
+                setattr(self, spec.name, child)
+            else:
+                # Since we know the children are all valid already,
+                # this must be a multi-element
+                existing_element.append(child)
+
+
+    def read_element_info(self, dataobj):
+        if dataobj.read(1):
+            dataobj.seek(-1, 1)
         else:
             # Now that we know that stream hasn't ended, go back a byte
             raise EOFError
 
-        e_id = ElementID(self.dataobj)
-        data_size = DataSize(self.dataobj)
-        data = self.dataobj.read(data_size)
-        return e_id, data_size, data
+        e_id = ElementID(dataobj)
+        data_size = DataSize(dataobj)
+        return e_id, data_size
 
     def write(self):
         """Calls write for all children, and concatenates the output into own
@@ -285,13 +329,12 @@ class Document(MasterElement):
         0x1A45DFA3: ElementSpec(0x1A45DFA3, 'ebml', EBMLHeader,
                                 multiple=True),
     }
-    
+
 
     def __init__(self, fileobj):
         self.children = []
 
-        self.dataobj = fileobj
-        self.read()
+        self.read(None, fileobj)
 
     def write(self):
         pass
