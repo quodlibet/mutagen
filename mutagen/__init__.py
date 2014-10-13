2694e2e0a9a206e1ee4c06ba686be917eb9591dc
Christoph Reiter <reiter.christoph@gmail.com>
2014-10-13 20:39:48 +0200

    version bump

--- mutagen/__init__.py
+++ mutagen/__init__.py
@@ -23,7 +23,7 @@ from mutagen._util import MutagenError
 from mutagen._file import FileType, StreamInfo, File
 from mutagen._tags import Metadata
 
-version = (1, 25, 1)
+version = (1, 25, 1, -1)
 """Version tuple."""
 
 version_string = ".".join(map(str, version))
