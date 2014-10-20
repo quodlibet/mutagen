a89b74792526f9f743ea2213e20532059bea9edb
Ben Ockmore <ben.sput@gmail.com>
2014-10-20 11:43:40 +0100

    _util.py: consistent headers/pep263

--- mutagen/_util.py
+++ mutagen/_util.py
@@ -1,4 +1,6 @@
-# Copyright 2006 Joe Wreschnig
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2006  Joe Wreschnig
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
