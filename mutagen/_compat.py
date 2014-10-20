19f7e02f24d10c05a95c2d199a3cd0dc229e2819
Ben Ockmore <ben.sput@gmail.com>
2014-10-20 11:42:08 +0100

    _compat.py: consistent headers/pep263

--- mutagen/_compat.py
+++ mutagen/_compat.py
@@ -1,4 +1,6 @@
-# Copyright 2013 Christoph Reiter
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013  Christoph Reiter
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
