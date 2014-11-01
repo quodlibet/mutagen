3ea3cc03bf1cedf23f24c5af5084b86e69ddee30
Ben Ockmore <ben.sput@gmail.com>
2014-11-01 23:10:06 +0000

    _ebml.py: introduced new ElementHeader class, updated classes to use it. Fixed all known issues.

--- mutagen/_ebml.py
+++ mutagen/_ebml.py
@@ -16,21 +16,58 @@ specification at:
 For an example of how to use it for specific formats, see the matroska.py file.
 """
 
-from ._compat import long_, text_type
-from mutagen._util import cdata, insert_bytes, delete_bytes
+from ._compat import integer_types, long_, text_type
+from ._util import cdata, delete_bytes, insert_bytes
 
 
 class EBMLParseError(Exception):
     pass
 
 
-class Element(object):
+class ElementHeader(object):
+    def __init__(self, _id, size, num_read_bytes=0):
+        self._id = _id
+        self._size = size
+        self._num_read_bytes = num_read_bytes
 
-    def __init__(self, _id, value):
-        self.id = _id
+    @property
+    def id(self):
+        return self._id
 
-        self.bytes_read = 0
-        self.bytes_read = self._get_byte_difference()
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, integer_types):
+            raise ValueError("ElementHeader.size must be an integer "
+                             "type.")
+
+        self._size = value
+
+    @classmethod
+    def read(cls, buf):
+        _id, _id_bytes = cls._read_id(buf)
+        _size, _size_bytes = cls._read_size(buf)
+
+        return cls(_id, _size, _id_bytes + _size_bytes)
+
+    def write(self, buf):
+        self._insert_or_delete_bytes(buf)
+        self._write_id(buf)
+        self._write_size(buf)
+
+    @property
+    def num_read_bytes(self):
+        return self._num_read_bytes
+
+    @property
+    def num_write_bytes(self):
+        return self._id_length() + self._size_length()
+
+    def byte_difference(self):
+        return self.num_write_bytes - self.num_read_bytes
 
     @staticmethod
     def _determine_vint_length(first_byte):
@@ -49,14 +86,14 @@ class Element(object):
 
         return 7-bval
 
-    @staticmethod
-    def _read_vint_bytes(buf, unset_ld):
+    @classmethod
+    def _get_vint_bytes(cls, buf, unset_ld):
         data = bytearray(buf.read(1))
 
         if not data:
             raise ValueError("Could not read first byte of {}".format(buf))
 
-        length_descriptor = Element._determine_vint_length(data[0])
+        length_descriptor = cls._determine_vint_length(data[0])
 
         if length_descriptor:
             data.extend(buf.read(length_descriptor))
@@ -67,61 +104,74 @@ class Element(object):
 
         return data
 
-    @staticmethod
-    def _vint_length(vint):
-        data = cdata.to_uint_be(vint)
-        return len(data.lstrip(b'\x00'))
-
-    @staticmethod
-    def _read_id(buf):
-        data = Element._read_vint_bytes(buf, unset_ld=False)
+    @classmethod
+    def _read_id(cls, buf):
+        data = cls._get_vint_bytes(buf, unset_ld=False)
 
         if len(data) > 4:
             raise EBMLParseError("More than four bytes read for element ID.")
+
+        read_length = len(data)
 
         # Pad to 4 bytes
         data[0:0] = b'\x00'*4
         data = data[-4:]
 
-        return cdata.uint_be(bytes(data))
+        return cdata.uint_be(bytes(data)), read_length
 
-    @staticmethod
-    def _write_id(buf, _id):
-        data = cdata.to_uint_be(_id)
+    def _write_id(self, buf):
+        data = cdata.to_uint_be(self._id)
         buf.write(data.lstrip(b'\x00'))
 
-    @staticmethod
-    def _read_size(buf):
-        data = Element._read_vint_bytes(buf, unset_ld=True)
+    def _id_length(self):
+        data = cdata.to_uint_be(self.id)
+        return len(data.lstrip(b'\x00'))
+
+    @classmethod
+    def _read_size(cls, buf):
+        data = cls._get_vint_bytes(buf, unset_ld=True)
 
         if len(data) > 8:
             raise EBMLParseError("More than eight bytes read for data size.")
+
+        read_length = len(data)
 
         # Pad to 8 bytes
         data[0:0] = b'\x00'*8
         data = data[-8:]
 
-        return cdata.ulonglong_be(bytes(data))
+        return cdata.ulonglong_be(bytes(data)), read_length
 
-    @staticmethod
-    def _write_size(buf, size):
-        data = cdata.to_ulonglong_be(size)
-        data = bytearray(data.lstrip(b'\x00'))
+    def _write_size(self, buf):
+
+        target_size = self._size_length()
+
+        data = cdata.to_ulonglong_be(self._size)
+        data = bytearray(data)
+        # Data should be padded to the target_size - since this is at minimum
+        # the size of the useful data, no need to worry about truncating
+        data[0:0] = b'\x00' * target_size
+        del data[:-target_size]
+
         length_descriptor = 0x80 >> (len(data) - 1)
         data[0] |= length_descriptor
+
         buf.write(data)
 
-    def bytes_to_write(self):
-        size = self.data_size
-        return (Element._vint_length(self.id) +
-                Element._vint_length(size) +
-                size)
+    def _size_length(self):
+        data = cdata.to_ulonglong_be(self._size)
+        data = bytearray(data.lstrip(b'\x00'))
+        length_descriptor = 0x80 >> (len(data) - 1)
 
-    def _get_byte_difference(self):
-        return self.bytes_to_write() - self.bytes_read
+        if data[0] >= length_descriptor:
+            data_length = len(data) + 1
+        else:
+            data_length = len(data)
+
+        return max(data_length, self.num_read_bytes - self._id_length())
 
     def _insert_or_delete_bytes(self, buf):
-        difference = self._get_byte_difference()
+        difference = self.byte_difference()
 
         # Insert into or delete byte from dataobj at the current
         # position to make room for content
@@ -130,97 +180,132 @@ class Element(object):
         elif difference < 0:
             delete_bytes(buf, -difference, buf.tell())
 
-        self.bytes_read += difference
+        self._num_read_bytes = self.num_write_bytes
 
-    def write(self, buf):
-        self._insert_or_delete_bytes(buf)
 
-        Element._write_id(buf, self.id)
-        Element._write_size(buf, self.data_size)
-        self._write_data(buf)
+class Element(object):
+
+    def __init__(self, header, value, num_read_bytes=0):
+        self._header = header
+
+        self._num_read_bytes = num_read_bytes
 
     @classmethod
-    def from_buffer(cls, _id, size, buf):
+    def read(cls, header, buf):
         raise NotImplementedError
+
+    def write(self, buf):
+        # Update Element size to reflect changes in content
+        self._header.size = self.num_write_bytes
+        self._header.write(buf)
+        self._insert_or_delete_bytes(buf)
+        self._write_data(buf)
 
     def _write_data(self, buf):
         raise NotImplementedError
 
     @property
-    def data_size(self):
+    def id(self):
+        return self._header.id
+
+    @property
+    def size(self):
+        return self._header.size
+
+    @property
+    def num_read_bytes(self):
+        return self._num_read_bytes
+
+    @property
+    def num_write_bytes(self):
         raise NotImplementedError
+
+    def byte_difference(self):
+        return self.num_write_bytes - self._num_read_bytes
+
+    def _insert_or_delete_bytes(self, buf):
+        difference = self.byte_difference()
+
+        # Insert into or delete byte from dataobj at the current
+        # position to make room for content
+        if difference > 0:
+            insert_bytes(buf, difference, buf.tell())
+        elif difference < 0:
+            delete_bytes(buf, -difference, buf.tell())
+
+        self._num_read_bytes = self.num_write_bytes
 
 
 class UnsignedElement(long_, Element):
 
-    def __new__(cls, _id, value):
+    def __new__(cls, header, value, num_read_bytes=0):
         return long_.__new__(UnsignedElement, value)
 
     @classmethod
-    def from_buffer(cls, _id, size, buf):
-        data = buf.read(size)
+    def read(cls, header, buf):
+        data = buf.read(header.size)
         # Pad the raw data to 8 octets
         data = (b'\x00'*8 + data)[-8:]
-        return cls(_id, cdata.ulonglong_be(data))
+        return cls(header, cdata.ulonglong_be(data), header.size)
 
     def _write_data(self, buf):
         data = cdata.to_ulonglong_be(self).strip(b'\x00')
         buf.write(data)
 
     @property
-    def data_size(self):
+    def num_write_bytes(self):
         return len(cdata.to_ulonglong_be(self).strip(b'\x00'))
 
 
 class ASCIIElement(text_type, Element):
-    def __new__(cls, _id, value):
+    def __new__(cls, header, value, num_read_bytes=0):
         return text_type.__new__(ASCIIElement, value)
 
     @classmethod
-    def from_buffer(cls, _id, size, buf):
-        data = buf.read(size)
-        return cls(_id, data.strip(b'\x00').decode('ascii'))
+    def read(cls, header, buf):
+        data = buf.read(header.size)
+        return cls(header, data.strip(b'\x00').decode('ascii'), header.size)
 
     def _write_data(self, buf):
         data = self.encode('ascii')
         buf.write(data)
 
     @property
-    def data_size(self):
+    def num_write_bytes(self):
         return len(self.encode('ascii'))
 
 
 class UTF8Element(text_type, Element):
-    def __new__(cls, _id, value):
+    def __new__(cls, header, value, num_read_bytes=0):
         return text_type.__new__(UTF8Element, value)
 
     @classmethod
-    def from_buffer(cls, _id, size, buf):
-        data = buf.read(size)
-        return cls(_id, data.strip(b'\x00').decode('utf8'))
+    def read(cls, header, buf):
+        data = buf.read(header.size)
+        return cls(header, data.strip(b'\x00').decode('utf8'), header.size)
 
     def _write_data(self, buf):
         data = self.encode('utf8')
         buf.write(data)
 
     @property
-    def data_size(self):
+    def num_write_bytes(self):
         return len(self.encode('utf8'))
 
 
 class BinaryElement(bytes, Element):
-    def __new__(cls, _id, value):
+    def __new__(cls, header, value, num_read_bytes=0):
         return bytes.__new__(BinaryElement, value)
 
     @classmethod
-    def from_buffer(cls, _id, size, buf):
-        return cls(_id, buf.read(size))
+    def read(cls, header, buf):
+        return cls(header, buf.read(header.size), header.size)
 
     def _write_data(self, buf):
         buf.write(self)
 
     @property
-    def data_size(self):
+    def num_write_bytes(self):
         return len(self)
 
 
@@ -241,38 +326,50 @@ class ElementSpec(object):
         self.mandatory = mandatory
         self.multiple = multiple
 
-    def create_element(self, e_id, d_size, dataobj):
+    def create_element(self, header, buf):
         """Creates a new element using this specification."""
 
-        return self.type.from_buffer(e_id, d_size, dataobj)
+        return self.type.read(header, buf)
 
 
 class MasterElement(Element):
     # Child elements, in format {ID: ElementSpec(...)}
     _child_specs = {}
 
-    def __init__(self, _id):
+    def __init__(self, header, num_read_bytes=0):
         self._children = []
 
-        super(MasterElement, self).__init__(_id, None)
+        super(MasterElement, self).__init__(header, None,
+                                            num_read_bytes=num_read_bytes)
 
-    def _write_children(self, buf):
+    @classmethod
+    def read(cls, header, buf):
+        result = cls(header, header.size)
+        result._read_children(buf)
+
+        return result
+
+    def _write_data(self, buf):
         # Since children insert or delete bytes as needed, no need to
         # resize file within MasterElement.write
         for child in self._children:
             # If the child was unrecognised, it can't have been
             # modified. Therefore, just skip this portion of the file.
-            if isinstance(child, tuple):
-                skip_size = child[1]
-                buf.seek(skip_size, 1)
+            if isinstance(child, ElementHeader):
+                buf.seek(child.size, 1)
             else:
                 child.write(buf)
 
-    # Override _get_byte_difference to ignore changes in child length, which
-    # are already dealt with when writing children
-    def _get_byte_difference(self):
-        return (Element._vint_length(self.id) +
-                Element._vint_length(self.data_size) - self.bytes_read)
+    @property
+    def num_write_bytes(self):
+        result = 0
+        for chld in self._children:
+            if isinstance(chld, Element):
+                result += (chld.num_write_bytes + chld._header.num_write_bytes)
+            else:
+                result += (chld.size + chld.num_write_bytes)
+
+        return result
 
     def _check_mandatory_children(self):
         # Check that all mandatory elements are set, otherwise raise Exception
@@ -310,60 +407,41 @@ class MasterElement(Element):
                 # this must be a multi-element
                 existing_element.append(child)
 
-    def _read(self, size, buf):
+    def _read_children(self, buf):
         bytes_read = 0
-        while bytes_read < size:
+        while bytes_read < self.size:
             try:
-                e_id = Element._read_id(buf)
+                header = ElementHeader.read(buf)
             except EOFError:
                 raise EBMLParseError('Not enough elements in EBML Master.')
 
-            d_size = Element._read_size(buf)
-            bytes_read += (Element._vint_length(e_id) +
-                           Element._vint_length(d_size) + d_size)
+            bytes_read += header.num_read_bytes
 
-            spec = self._child_specs.get(e_id, None)
+            spec = self._child_specs.get(header.id, None)
             if spec is None:
-                # Append tuple indicating element was unrecognised, then
-                # move on to the next element.
-                self._children.append((e_id, d_size))
-                buf.seek(d_size, 1)
-                continue
+                # Append ElementHeader, indicating element was unrecognised,
+                # then move on to the next element.
+                self._children.append(header)
+                buf.seek(header.size, 1)
+            else:
+                if not spec.multiple:
+                    # Check that there are no elements with the same ID
+                    if any(child.id == spec.id for child in self._children):
+                        raise Exception("Attempt to set multiple values for "
+                                        "non-multi element")
 
-            if not spec.multiple:
-                # Check that there are no elements with the same ID
-                if any(child.id == spec.id for child in self._children):
-                    raise Exception("Attempt to set multiple values for "
-                                    "non-multi element")
+                new_element = spec.create_element(header, buf)
+                self._children.append(new_element)
 
-            new_element = spec.create_element(e_id, d_size, buf)
-            self._children.append(new_element)
+            bytes_read += header.size
 
         self._check_mandatory_children()
         self._set_attributes()
 
-    @classmethod
-    def from_buffer(cls, _id, size, buf):
-        result = cls(_id)
-
-        result._read(size, buf)
-        return result
-
-    def _write_data(self, buf):
-        # Since children insert or delete bytes as needed, no need to
-        # resize file within MasterElement.write
-        for child in self._children:
-            # If the child was unrecognised, it can't have been
-            # modified. Therefore, just skip this portion of the file.
-            if isinstance(child, tuple):
-                skip_size = child[1]
-                buf.seek(skip_size, 1)
-            else:
-                child.write(buf)
-
-    @property
-    def data_size(self):
-        return sum(child.bytes_to_write() for child in self._children)
+    # Override byte_difference to ignore changes in child length, which are
+    # already dealt with when writing children
+    def byte_difference(self):
+        return 0
 
 
 class EBMLHeader(MasterElement):
