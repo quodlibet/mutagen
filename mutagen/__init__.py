28d6b27137656c8ec5896c7eacf50d2ab498020c
Christoph Reiter <reiter.christoph@gmail.com>
2016-05-02 20:53:47 +0200

    release

--- mutagen/__init__.py
+++ mutagen/__init__.py
@@ -24,7 +24,7 @@ from mutagen._util import MutagenError
 from mutagen._file import FileType, StreamInfo, File
 from mutagen._tags import Tags, Metadata, PaddingInfo
 
-version = (1, 31, -1)
+version = (1, 32)
 """Version tuple."""
 
 version_string = ".".join(map(str, version))
