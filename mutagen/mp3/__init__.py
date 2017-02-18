8a363fce5a2498aa0f5bfcea94e9af9b3b3a2362
Christoph Reiter <reiter.christoph@gmail.com>
2017-02-18 16:15:28 +0100

    MP3: fix error with xing frames without a frame count. Fixes #292

--- mutagen/mp3/__init__.py
+++ mutagen/mp3/__init__.py
@@ -194,8 +194,8 @@ class MPEGFrame(object):
                     # files with low bitrate
                     samples = 0
                 self.length = float(samples) / self.sample_rate
-            if xing.bytes != -1 and self.length:
-                self.bitrate = int((xing.bytes * 8) / self.length)
+                if xing.bytes != -1 and self.length:
+                    self.bitrate = int((xing.bytes * 8) / self.length)
             if xing.lame_version_desc:
                 self.encoder_info = u"LAME %s" % xing.lame_version_desc
             if lame is not None:
