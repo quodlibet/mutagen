97672806776e9d52d21e16f8a247e7e51a0d634d
Christoph Reiter <reiter.christoph@gmail.com>
2017-12-28 12:58:35 +0100

    docs: Work around docutils URL parsing bug
    See https://github.com/sphinx-doc/sphinx/issues/4350

--- mutagen/trueaudio.py
+++ mutagen/trueaudio.py
@@ -10,7 +10,7 @@
 
 True Audio is a lossless format designed for real-time encoding and
 decoding. This module is based on the documentation at
-http://www.true-audio.com/TTA_Lossless_Audio_Codec_-_Format_Description
+http://www.true-audio.com/TTA_Lossless_Audio_Codec\_-_Format_Description
 
 True Audio files use ID3 tags.
 """
