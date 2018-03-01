fd74f076071e510362e2e63f6f80400d5899a6a8
Borewit <borewit@users.noreply.github.com>
2018-03-01 19:52:56 +0100

    wave: Add a unit test mocking Picard saving an ID3 to WAVE

--- tests/test_wave.py
+++ tests/test_wave.py
@@ -8,29 +8,29 @@ from tests import TestCase, DATA_DIR, get_temp_copy
 
 class TWave(TestCase):
     def setUp(self):
-        fn_wav_pcm_2s_16000_08_ID3v23 = \
+        fn_wav_pcm_2s_16000_08_id3v23 = \
             os.path.join(DATA_DIR, "silence-2s-PCM-16000-08-ID3v23.wav")
         self.wav_pcm_2s_16000_08_ID3v23 = \
-            WAVE(fn_wav_pcm_2s_16000_08_ID3v23)
+            WAVE(fn_wav_pcm_2s_16000_08_id3v23)
 
         self.tmp_fn_pcm_2s_16000_08_ID3v23 = \
-            get_temp_copy(fn_wav_pcm_2s_16000_08_ID3v23)
+            get_temp_copy(fn_wav_pcm_2s_16000_08_id3v23)
         self.tmp_wav_pcm_2s_16000_08_ID3v23 = \
             WAVE(self.tmp_fn_pcm_2s_16000_08_ID3v23)
 
-        fn_wav_pcm_2s_16000_08_notags = \
+        self.fn_wav_pcm_2s_16000_08_notags = \
             os.path.join(DATA_DIR, "silence-2s-PCM-16000-08-notags.wav")
         self.wav_pcm_2s_16000_08_notags = \
-            WAVE(fn_wav_pcm_2s_16000_08_notags)
+            WAVE(self.fn_wav_pcm_2s_16000_08_notags)
 
         self.tmp_fn_pcm_2s_16000_08_notag = \
-            get_temp_copy(fn_wav_pcm_2s_16000_08_notags)
+            get_temp_copy(self.fn_wav_pcm_2s_16000_08_notags)
         self.tmp_wav_pcm_2s_16000_08_notag = \
             WAVE(self.tmp_fn_pcm_2s_16000_08_notag)
 
-        fn_wav_pcm_2s_44100_16_ID3v23 = \
+        fn_wav_pcm_2s_44100_16_id3v23 = \
             os.path.join(DATA_DIR, "silence-2s-PCM-44100-16-ID3v23.wav")
-        self.wav_pcm_2s_44100_16_ID3v23 = WAVE(fn_wav_pcm_2s_44100_16_ID3v23)
+        self.wav_pcm_2s_44100_16_ID3v23 = WAVE(fn_wav_pcm_2s_44100_16_id3v23)
 
     def test_channels(self):
         self.failUnlessEqual(self.wav_pcm_2s_16000_08_ID3v23.info.channels, 2)
@@ -65,7 +65,7 @@ class TWave(TestCase):
         self.failUnless("audio/wav" in self.wav_pcm_2s_44100_16_ID3v23.mime)
         self.failUnless("audio/wave" in self.wav_pcm_2s_44100_16_ID3v23.mime)
 
-    def test_ID3_tags(self):
+    def test_id3_tags(self):
         id3 = self.wav_pcm_2s_44100_16_ID3v23.tags
         self.assertEquals(id3["TALB"], "Quod Libet Test Data")
         self.assertEquals(id3["TCON"], "Silence")
@@ -104,10 +104,90 @@ class TWave(TestCase):
         new = WAVE(self.tmp_wav_pcm_2s_16000_08_ID3v23.filename)
         self.failUnlessEqual(new["TIT1"], ["foobar"])
 
-    def test_save_without_ID3_chunk(self):
-        from mutagen.id3 import TIT1
-        self.tmp_wav_pcm_2s_16000_08_notag["TIT1"] = TIT1(encoding=3,
-                                                          text="foobar")
-        self.tmp_wav_pcm_2s_16000_08_notag.save()
-        self.failUnless(WAVE(self.tmp_fn_pcm_2s_16000_08_notag)["TIT1"]
-                        == "foobar")
+    """" Simulate the way Picard writes and update tags """
+    def test_picard_lifecycle(self):
+        path_tmp_wav_file = \
+            get_temp_copy(self.fn_wav_pcm_2s_16000_08_notags)
+        from mutagen.id3 import ID3
+        wav = WAVE(path_tmp_wav_file)
+        id3 = wav.tags
+        """" Picard WaveFile._get_tags: """
+        self.assertIsNone(id3, "Ensure ID3-tag-header does not exist")
+        """" Picard WaveFile._get_tags: initialize tags """
+        wav.add_tags()
+        id3 = wav.tags
+        self.assertIsInstance(id3, ID3)
+        """ ID3v2.3 separator """
+        separator = '/'
+        """ Initialize Picard like metadata tags """
+        self.__init_id3_tags(id3, major=3)
+        """ Write the Picard like metadata to the empty WAVE-file """
+        id3.save(path_tmp_wav_file, v23_sep=separator)
+        """ Tags (metadata) have been added; now load the file again """
+        wav = WAVE(path_tmp_wav_file)
+        id3 = wav.tags
+        self.assertIsInstance(id3, ID3)
+        self.assertEquals(id3["TRCK"], "1/10")
+        self.assertEquals(id3["TPOS"], "1/1")
+        self.assertEquals(id3["TXXX:MusicBrainz Release Group Id"], "e00305af-1c72-469b-9a7c-6dc665ca9adc")
+        self.assertEquals(id3["TXXX:MusicBrainz Album Artist Id"],
+                          ["3fe817fc-966e-4ece-b00a-76be43e7e73c", "984f8239-8fe1-4683-9c54-10ffb14439e9"])
+        self.assertEquals(id3["TXXX:CATALOGNUMBER"], ["PRAR931391"])
+        self.assertEquals(id3["TSRC"], ["NLB931100460", "USMH51100098"])
+
+    @staticmethod
+    def __init_id3_tags(id3, major=3):
+        """
+        Attributes:
+            id3 ID3 Tag object
+            major ID3 major version, e.g.: 3 for ID3v2.3
+        """
+        from mutagen.id3 import TRCK, TPOS, TXXX, TPUB, TALB, UFID, TPE2, TSO2, TMED, TIT2, TPE1, TSRC, IPLS, TORY, \
+            TDAT, TYER
+        id3.add(TRCK(encoding=major, text="1/10"))
+        id3.add(TPOS(encoding=major, text="1/1"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Release Group Id",
+                     text="e00305af-1c72-469b-9a7c-6dc665ca9adc"))
+        id3.add(TXXX(encoding=major, desc="originalyear", text="2011"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Album Type", text="album"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Album Id", text="e7050302-74e6-42e4-aba0-09efd5d431d8"))
+        id3.add(TPUB(encoding=major, text="J&R Adventures"))
+        id3.add(TXXX(encoding=major, desc="CATALOGNUMBER", text="PRAR931391"))
+        id3.add(TALB(encoding=major, text="Don\'t Explain"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Album Status", text="official"))
+        id3.add(TXXX(encoding=major, desc="SCRIPT", text="Latn"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Album Release Country", text="US"))
+        id3.add(TXXX(encoding=major, desc="BARCODE", text="804879313915"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Album Artist Id",
+                     text=["3fe817fc-966e-4ece-b00a-76be43e7e73c", "984f8239-8fe1-4683-9c54-10ffb14439e9"]))
+        id3.add(TPE2(encoding=major, text="Beth Hart & Joe Bonamassa"))
+        id3.add(TSO2(encoding=major, text="Hart, Beth & Bonamassa, Joe"))
+        id3.add(TXXX(encoding=major, desc="ASIN", text="B005NPEUB2"))
+        id3.add(TMED(encoding=major, text="CD"))
+        id3.add(UFID(encoding=major, owner="http://musicbrainz.org", data=b"f151cb94-c909-46a8-ad99-fb77391abfb8"))
+        id3.add(TIT2(encoding=major, text="Sinner's Prayer"))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Artist Id",
+                     text=["3fe817fc-966e-4ece-b00a-76be43e7e73c", "984f8239-8fe1-4683-9c54-10ffb14439e9"]))
+        id3.add(TPE1(encoding=major, text=["Beth Hart & Joe Bonamassa"]))
+        id3.add(TXXX(encoding=major, desc="Artists", text=["Beth Hart", "Joe Bonamassa"]))
+        id3.add(TSRC(encoding=major, text=["NLB931100460", "USMH51100098"]))
+        id3.add(TXXX(encoding=major, desc="MusicBrainz Release Track Id",
+                     text="d062f484-253c-374b-85f7-89aab45551c7"))
+        id3.add(IPLS(encoding=major, people=[
+            ["engineer", "James McCullagh"],
+            ["engineer", "Jared Kvitka"],
+            ["arranger", "Jeff Bova"],
+            ["producer", "Roy Weisman"],
+            ["piano", "Beth Hart"],
+            ["guitar", "Blondie Chaplin"],
+            ["guitar", "Joe Bonamassa"],
+            ["percussion", "Anton Fig"],
+            ["drums", "Anton Fig"],
+            ["keyboard", "Arlan Schierbaum"],
+            ["bass guitar", "Carmine Rojas"],
+            ["orchestra", "The Bovaland Orchestra"],
+            ["vocals", "Beth Hart"],
+            ["vocals", "Joe Bonamassa"]])),
+        id3.add(TORY(encoding=major, text="2011"))
+        id3.add(TYER(encoding=major, text="2011"))
+        id3.add(TDAT(encoding=major, text="2709"))
