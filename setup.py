ef63e2dc084fc841da45aca17a9c8e9647ba4ed4
Christoph Reiter <reiter.christoph@gmail.com>
2019-11-17 17:51:17 +0100

    Note Python 2.8 support

--- setup.py
+++ setup.py
@@ -263,6 +263,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy',
             ('License :: OSI Approved :: '
