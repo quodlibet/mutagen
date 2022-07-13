577ea7fb7875788b8f35a5cde6f015b6532d31e0
destroid11 <destroid11@hotmail.com>
2022-07-13 17:37:20 -0400

    Correct bitrate

--- mutagen/wave.py
+++ mutagen/wave.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2017  Borewit
 # Copyright (C) 2019-2020  Philipp Wolfer
 #
@@ -88,7 +89,7 @@ class WaveStreamInfo(StreamInfo):
         info = struct.unpack('<hhLLhh', data[:self.SIZE])
         self.audio_format, self.channels, self.sample_rate, byte_rate, \
             block_align, self.bits_per_sample = info
-        self.bitrate = self.channels * block_align * self.sample_rate
+        self.bitrate = self.channels * self.bits_per_sample * self.sample_rate
 
         # Calculate duration
         self._number_of_samples = 0
